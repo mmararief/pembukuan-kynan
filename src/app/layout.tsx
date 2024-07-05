9ea2a2b3a2363874a43877782da63ba40f03b624
ammar arief <ammararief321@gmail.com>
2024-07-05 16:13:00 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Header from "@/components/header";
+import HeaderMobile from "@/components/header-mobile";
+import SideNav from "@/components/side-nav";
+import PageWrapper from "@/components/page-wrapper";
+import MarginWidthWrapper from "@/components/margin-width-wrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={` bg-white ${inter.className}`}>
+        <div className="flex">
+          <SideNav />
+          <main className="flex-1">
+            <MarginWidthWrapper>
+              <Header />
+              <HeaderMobile />
+              <PageWrapper>{children}</PageWrapper>
+            </MarginWidthWrapper>
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/app/messages/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MassagesPage = () => {
+  return <div>MassagesPage</div>;
+};
+
+export default MassagesPage;

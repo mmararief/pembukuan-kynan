f06dda3a0147e658235ba2fcf46585bf3f303081
lopingsuee <umaygans03@gmail.com>
2024-07-15 22:24:22 +0700

    status whatsapp terhubung atau tidak

--- src/components/WaStatus.tsx
+++ src/components/WaStatus.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const WaStatus = () => {
-  return <div>WaStatus</div>;
+  
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/auth-status`);
+        setIsAuthenticated(response.data.authenticated);
+      } catch (err) {
+        console.error("Failed to check auth status", err);
+        setError("Failed to check auth status");
+      }
+    };
+
+    checkAuthStatus();
+
+    if (!apiUrl) {
+      setError("API URL is not defined");
+      return;
+    }
+  }, [apiUrl]);
+
+  return (
+    
+  <div className={` flex justify-center font-bold rounded-full max-w-[15rem] p-2  ${
+    isAuthenticated ? 'bg-green-200 border border-green-500 text-black ' : 'bg-red-200 border text-sm border-red-500 text-black'
+  }`}>
+     <div
+          className={`w-4 h-4 rounded-full mt-1 mr-2 ${
+            isAuthenticated ? 'bg-green-500' : 'bg-red-500'
+          }`}
+        />
+    <div className="">
+          {isAuthenticated ? "Whatsapp Tersambung" : "Whatsapp tidak Tersambung"} 
+        </div>
+
+
+  </div>
+
+
+  )
 };
 
 export default WaStatus;
+function setError(arg0: string) {
+  throw new Error("Function not implemented.");
+}
+

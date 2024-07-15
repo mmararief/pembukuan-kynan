8c0b20c4f8af1d61a8571e5d1b034dcd276153d0
ammar arief <ammararief321@gmail.com>
2024-07-15 13:28:55 +0700

    update pembukuan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import WaStatus from "@/components/WaStatus";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hi this is home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold p-4">Home</h1>
+      <WaStatus />
+    </div>
+  );
 }
---
+++ src/components/WaStatus.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WaStatus = () => {
+  return <div>WaStatus</div>;
+};
+
+export default WaStatus;

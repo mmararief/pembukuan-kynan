0890b82a27b5c8ca91f055be7577ffebc654a921
ammar arief <ammararief321@gmail.com>
2024-07-26 14:53:56 +0700

    save types

--- src/styles/types.tsx
+++ src/styles/types.tsx
@@ -10,3 +10,37 @@ export type MenuItemWithSubMenuProps = {
   item: SideNavItem;
   toggleOpen: () => void;
 };
+
+// types.ts
+export interface Produk {
+  id_produk: number;
+  id_kategori: number;
+  nama_produk: string;
+  status: string;
+  gambar: string;
+  harga_jual: string;
+  hpp: string | null; // Ensure this is consistent
+}
+
+export interface DetailTransaksi {
+  id_detail: number;
+  id_transaksi: number;
+  id_produk: number;
+  jumlah: number;
+  harga: string;
+  subtotal: string;
+  produk: Produk;
+}
+
+export interface Transaksi {
+  id_transaksi: number;
+  tanggal: string;
+  via: string;
+  nama: string;
+  whatsapp: string;
+  alamat: string;
+  metode_pembayaran: string;
+  total: string;
+  status: string;
+  detailtransaksi: DetailTransaksi[];
+}
